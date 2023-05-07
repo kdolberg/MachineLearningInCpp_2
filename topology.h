88f77d4892fc17ee87e1d04fd2a10e024c62ff59
Kris Dolberg <kristofor.d@gmail.com>
2023-05-07 15:51:49 -0700

    I think this is finally in a useful state

--- topology.h
+++ topology.h
@@ -3,52 +3,28 @@
 
 #include <vector>
 #include <iostream>
-
-#define NUM_INPUT_LAYERS 1
+// #include "../CppUtilities/vec_overload.h"
 
 typedef unsigned int uint;
-
-/** @brief A class template for a neural network, and all sorts of data associated with it.
- * 
+/**
+ * @brief Simple class template for neural networks and all data types that must have the same topology.
  */
-template <class T>
-class Topology<T> : public std::vector<std::vector<T>> {
-public:
-	Topology(const uint * definition, uint length);
-	Topology(const std::vector<uint> definition);
-	~Topology();
-};
-
-// template <typename T>
-// std::ostream& operator<<(std::ostream& os,const Topology<double>& t) {
-// 	for (Topology<double>::const_iterator i = t.cbegin(); i != t.cend(); ++i) {
-// 		os << "{"
-// 		for (std::vector<double>::const_iterator j = i->cbegin(); j != i->cend(); ++j) {
-// 			os << " " << *j << " ";
-// 		}
-// 		os << "}\n";
-// 	}
-// 	return os;
-// }
-
-#endif
-
-
-#ifndef TOPOLOGY_H
-#define TOPOLOGY_H
-
-#include <vector>
-#include <iostream>
-#define NUM_INPUT_LAYERS 1
-typedef unsigned int uint;
-
 template <typename T>
 class Topology : public std::vector<std::vector<T>> {
 public:
-    Topology(const uint *definition, uint length);
-    Topology(const std::vector<uint> definition);
-    ~Topology();
-    // Other member functions and data members
+	/**
+	 * @brief Standard constructor for this class.
+	 * @param *definition Defines how many elements are in each sub-array.
+	 * @param length Defines the length of the definition array.
+	 */
+    Topology(const uint *definition, uint length) {
+		this->resize(length);
+		for (uint i = 0; i < this->size(); ++i) {
+			(*this)[i].resize(definition[i]);
+	    }
+	}
+    Topology(const std::vector<uint> definition) : Topology(definition.data(), definition.size()) {}
+    ~Topology() {}
 };
 
 #endif // TOPOLOGY_H
