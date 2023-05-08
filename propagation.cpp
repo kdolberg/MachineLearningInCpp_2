0c2a7c292b16ee5f7a35d0eee529256ff2d98fac
Kris Dolberg <kristofor.d@gmail.com>
2023-05-08 09:57:16 -0700

    Added propagation.cpp

---
+++ propagation.cpp
@@ -0,0 +1,35 @@
+#include "propagation.h"
+
+void make_wb_topology(const net_wb_t& wb, )
+
+/**
+ * @brief Allocates memory for a forward data cache struct based on net
+ * @param net The neural network that will be using this data cache
+ * @param fdata The forward data cache struct whose memory will be allocated 
+ */
+void allocate_forward_data_cache_memory(const net_t& net, net_forward_data_cache_t& fdata, net_back_data_cache_t& bdata) {
+    make_topology(fdata.layer_io,net);
+    make_topology(fdata.naive_partials,net);
+    for (uint i = 0; i < wb.size(); ++i) {
+        
+    }
+}
+
+void allocate_back_data_cache_memory(const net_t& net, net_forward_data_cache_t& fdata, net_back_data_cache_t& bdata) {
+    make_topology(fdata.layer_io,net);
+    make_topology(fdata.naive_partials,net);
+    for (uint i = 0; i < wb.size(); ++i) {
+        
+    }
+}
+
+void forwardpropagate(const net_t& net,net_forward_data_cache_t& fdata) {
+
+    /*Some additional memory allocation needed for naive partials*/
+    for (uint i = 0; i < net.wb.size(); ++i) {
+        net[i]*fdata.layer_io[i];
+    }
+}
+
+void backpropagate(const net_t& net, const net_forward_data_cache_t& fdata, net_back_data_cache_t& bdata) {
+}
