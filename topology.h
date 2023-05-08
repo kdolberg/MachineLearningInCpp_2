a38f96d5bcf2fe92bf672e45beec3268c8bf185c
Kris Dolberg <kristofor.d@gmail.com>
2023-05-07 17:52:45 -0700

    I think I have this working

--- topology.h
+++ topology.h
@@ -2,29 +2,45 @@
 #define TOPOLOGY_H
 
 #include <vector>
-#include <iostream>
-// #include "../CppUtilities/vec_overload.h"
+#include <cstddef>
 
+#ifndef uint
 typedef unsigned int uint;
+#endif
+
 /**
- * @brief Simple class template for neural networks and all data types that must have the same topology.
+ * @brief Function template that provides the ability to format any arbitrary 2-D vector.
+ * @param top Reference to a previously-initialized std::vector<std::vector<T>> to be formatted.
+ * @param def Vector specifiying the length of each sub-vector.
  */
 template <typename T>
-class Topology : public std::vector<std::vector<T>> {
-public:
-	/**
-	 * @brief Standard constructor for this class.
-	 * @param *definition Defines how many elements are in each sub-array.
-	 * @param length Defines the length of the definition array.
-	 */
-    Topology(const uint *definition, uint length) {
-		this->resize(length);
-		for (uint i = 0; i < this->size(); ++i) {
-			(*this)[i].resize(definition[i]);
-	    }
+void make_topology(std::vector<std::vector<T>>& top,const std::vector<uint>& def) {
+	top.resize(def.size());
+	for (uint i = 0; i < top.size(); ++i) {
+		top[i].resize(def[i]);
 	}
-    Topology(const std::vector<uint> definition) : Topology(definition.data(), definition.size()) {}
-    ~Topology() {}
-};
+}
 
-#endif // TOPOLOGY_H
+/**
+ * @brief Function template that provides the ability to format any arbitrary 2-D vector.
+ * @param top Pointer to a previously-initialized std::vector<std::vector<T>> to be formatted. Please note that the function assumes that memory is allocated for this object.
+ * @param def Vector specifiying the length of each sub-vector.
+ */
+template <typename W>
+void make_topology(std::vector<std::vector<W>> * top,const std::vector<uint>& def) {
+	assert(top!=NULL);
+	make_topology(*top,def);
+}
+
+/**
+ * @brief Function template that provides the ability to make any arbitrary 2-D vector
+ * @param def Vector specifiying the length of each sub-vector.
+ */
+template <typename U>
+std::vector<std::vector<U>> make_topology(const std::vector<uint>& def) {
+	std::vector<std::vector<U>> ret;
+	make_topology(ret,def);
+	return ret;
+}
+
+#endif
