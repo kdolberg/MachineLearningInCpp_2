61a21b9e061d26f66d1537255843d85890c59567
Kris Dolberg <kristofor.d@gmail.com>
2023-05-08 11:14:34 -0700

    Updated as per new datatype and file names

--- overloads.cpp
+++ overloads.cpp
@@ -3,20 +3,19 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#include "node.h"
-#include "data.h"
+#include "datatypes.h"
 
-std::vector<scalar_t> for_each_scalar(const scalar_function_t& af,const std::vector<scalar_t>& input) {
-	std::vector<scalar_t> ret(input.size());
+std::vector<scalar_datum_t> for_each_scalar(const scalar_function_t& af,const std::vector<scalar_datum_t>& input) {
+	std::vector<scalar_datum_t> ret(input.size());
 	for (uint32_t i = 0; i < input.size(); ++i) {
 		ret[i] = af(input[i]);
 	}
 	return ret;
 }
 
-scalar_t operator*(const node_wb_t& n, const vector_datum_t& input) {
+scalar_datum_t operator*(const node_wb_t& n, const vector_datum_t& input) {
 	assert(n.w.size()==input.size());
-	scalar_t ret = n.b;
+	scalar_datum_t ret = n.b;
 	for (uint32_t i = 0; i < n.w.size(); ++i)
 	{
 		ret += n.w[i]*input[i];
@@ -24,7 +23,7 @@ scalar_t operator*(const node_wb_t& n, const vector_datum_t& input) {
 	return ret;
 }
 
-std::ostream& operator<<(std::ostream& os, std::vector<scalar_t> v) {
+std::ostream& operator<<(std::ostream& os, std::vector<scalar_datum_t> v) {
 	os << "{";
 	for (uint32_t i = 0; i < v.size(); ++i) {
 		os << v[i];
