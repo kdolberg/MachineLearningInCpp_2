64d9a7f0cfc327583c106aa9fbb82b6891b57874
Kris Dolberg <kristofor.d@gmail.com>
2023-05-08 09:59:09 -0700

    Comment fix

--- propagation.h
+++ propagation.h
@@ -27,7 +27,7 @@ void forwardpropagate(const net_t& net,net_forward_data_cache_t& fdata);
  * @brief Perforforms a single iteration of backpropagation
  * Only works of forwardpropagate has been run on this net first.
  * @param net Neural net that is being backpropagated
- * @param fdata Forward propagation data cache that would have been calculated using forwardpropagate
+ * @param fdata Forward propagation data cache previously calculated using forwardpropagate
  * @param bdata Data cache for all outputs during this process.
  */
 void backpropagate(const net_t& net, const net_forward_data_cache_t& fdata, net_back_data_cache_t& bdata);
