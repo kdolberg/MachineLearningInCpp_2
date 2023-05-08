b57d0be1e9ccdffe167ae8b7f837cd64aa4be3cb
Kris Dolberg <kristofor.d@gmail.com>
2023-05-08 12:52:48 -0700

    Added a whole bunch of new function templates to maximize generalizability

--- topology.h
+++ topology.h
@@ -16,6 +16,63 @@ typedef unsigned int uint;
 #endif
 
 /**
+ * @brief Non-recursively resizes u to be the same size as t
+ * @param u Destination vector
+ * @param t Source vector
+ */
+template <typename U,typename T>
+void copy_1d_topology(std::vector<U>& u,const std::vector<T>& t) {
+	u.resize(t.size());
+}
+
+/**
+ * @brief A "stub" function template that does nothing.
+ * Supports the other function with the same name. Avoids an error when we get to the end of the recursive arrays.
+ */
+template <typename U,typename T>
+int copy_topology(const U& u,const T& t,uint new_size) {
+	assert(new_size==0);
+	return 0;
+}
+
+/**
+ * @brief Non-recursively resizes u to match new_size
+ * @param u Vector to be resized
+ * @param t Dummy argument. Does nothing
+ * @param new_size New size for u 
+ */
+template <typename U,typename T>
+void copy_topology(std::vector<U>& u,const T& t,uint new_size) {
+	if(new_size!=0){
+		u.resize(new_size);
+	}
+}
+
+/**
+ * @brief Recursively copies the dimensions of two arbitrarily-dimensioned arrays
+ * @param u Destination vector
+ * @param t Source vector
+ * @param new_size Optional.
+ */
+template <typename U,typename T>
+void copy_topology(std::vector<U>& u,const std::vector<T>& t, uint new_size) {
+	copy_1d_topology(u,t);
+	for (uint i = 0; i < u.size(); ++i) {
+		copy_topology(u[i],t[i],new_size);
+	}
+}
+
+/**
+ * @brief Recursively copies the dimensions of two arbitrarily-dimensioned arrays
+ * @param u Destination vector
+ * @param t Source vector
+ */
+template <typename U,typename T>
+void copy_topology(std::vector<U>& u,const std::vector<T>& t) {
+	copy_topology(u,t,0);
+}
+
+/**
  * @brief Function template that provides the ability to format any arbitrary 2-D vector.
  * @param top Reference to a previously-initialized std::vector<std::vector<T>> to be formatted.
  * @param def Vector specifiying the length of each sub-vector.
