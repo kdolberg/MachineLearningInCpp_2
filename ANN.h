59566e6b1c41a34a609e9ce3836c61218523e09c
Kris Dolberg <kristofor.d@gmail.com>
2023-05-07 18:51:28 -0700

    Header for the ANN class

---
+++ ANN.h
@@ -0,0 +1,30 @@
+#ifndef ANN_H
+#define ANN_H
+
+#include "propagation.h"
+#include "topology.h"
+
+class ANN {
+private:
+	net_t net;
+	net_forward_data_cache_t forward_data;
+	net_back_data_cache_t back_data;
+public:
+	ANN(unsigned int inputs, std::vector<unsigned int> def);
+	~ANN();
+	/**
+	 * Saves the xy_dataset in the net.
+	 */
+	void load_training_data(const xy_dataset_t& dataset);
+	/**
+	 * @brief Performs one iteration of forward and backpropagation on the current xy_dataset_t
+	 */
+	void learn();
+private:
+	/**
+	 * @brief Allocates memory for the back and forward data caches.
+	 */
+	void initialize_data_caches();
+};
+
+#endif
