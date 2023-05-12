1e81f33748609f15c77123f17f36e17b72536fbc
Kris Dolberg <kristofor.d@gmail.com>
2023-05-12 10:17:51 -0700

    Added include

--- overloads.h
+++ overloads.h
@@ -2,6 +2,7 @@
 #define OVERLOADS_H
 
 #include "datatypes.h"
+#include <ostream>
 #include "../CppUtilities/vec_overload.h"
 
 template<typename T>
@@ -20,14 +21,84 @@ std::vector<T>& operator+=(std::vector<T>& a, const std::vector<T>& b) {
 }
 
 template<typename T>
+std::vector<T>& operator-=(std::vector<T>& a, const std::vector<T>& b) {
+	assert(a.size()==b.size());
+	for (uint i = 0; i < a.size(); ++i) {
+		a[i] -= b[i];
+	}
+	return a;
+}
+
+template<typename T>
+std::vector<T> operator-(const std::vector<T>& a, const std::vector<T>& b) {
+	std::vector<T> ret(a.cbegin(),a.cend());
+	ret-=b;
+	return ret;
+}
+
+template<typename T>
 node_shape_s<T>& operator+=(node_shape_s<T>& a, const node_shape_s<T>& b) {
 	a.w += b.w;
 	a.b += b.b;
 	return a;
 }
 
-std::ostream& operator<<(std::ostream& os, const net_wb_t& wb);
+template<typename T>
+node_shape_s<T>& operator-=(node_shape_s<T>& a, const node_shape_s<T>& b) {
+	a.w -= b.w;
+	a.b -= b.b;
+	return a;
+}
 
+template<typename T>
+std::vector<T>& operator*=(std::vector<T>& t, const T& b){
+	assert(!(t.empty()));
+	for (uint i = 0; i < t.size(); ++i) {
+		t[i]*=b;
+	}
+	return t;
+}
+
+template<typename T>
+std::vector<T>& operator*=(std::vector<T>& a, const std::vector<T>& b) {
+	assert(a.size()==b.size());
+	for (uint i = 0; i < a.size(); ++i) {
+		a[i] *= b[i];
+	}
+	return a;
+}
+
+template<typename T>
+std::vector<T> operator*(const std::vector<T>& a, const std::vector<T>& b) {
+	std::vector<T> ret(a.cbegin(),a.cend());
+	ret *= b;
+	return ret;
+}
+
+template<typename T>
+std::vector<std::vector<T>>& operator*=(std::vector<std::vector<T>>& t, const T& b) {
+	assert(!(t.empty()));
+	for (uint i = 0; i < t.size(); ++i) {
+		t[i]*=b;
+	}
+	return t;
+}
+
+template<typename T>
+std::vector<T> operator*(const std::vector<T>& t, const T& b) {
+	std::vector<T> ret(t.cbegin(),t.cend());
+	ret*=b;
+	return ret;
+}
+
+template<typename T>
+std::vector<std::vector<T>> operator*(const std::vector<std::vector<T>>& t, const T& b) {
+	std::vector<std::vector<T>> ret(t.cbegin(),t.cend());
+	ret*=b;
+	return ret;
+}
+
+std::ostream& operator<<(std::ostream& os, const net_wb_t& wb);
 std::ostream& operator<<(std::ostream& os, const net_t& net);
 std::ostream& operator<<(std::ostream& os, const xy_dataset_t& xy);
 
