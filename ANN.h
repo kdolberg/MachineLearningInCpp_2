035c964d0f82264a55850a356ab1a5e5b619fe2f
Kris Dolberg <kristofor.d@gmail.com>
2023-05-12 10:36:11 -0700

    Added funcs

--- ANN.h
+++ ANN.h
@@ -12,14 +12,29 @@
 /**
  * @brief Produces a random scalar_datum_t between -0.5 and 0.5
  */
-scalar_datum_t random_scalar();
+static scalar_datum_t random_scalar();
 
 /**
  * @brief Initializes a net_t object
+ * ReLU is assigned to all layers
  * @param n The net to be formatted
  * @param def The definition of the net
  */
-void make_net(net_t& n, const std::vector<uint>& def);
+/*static*/ void make_net(net_t& n, const std::vector<uint>& def);
+
+/**
+ * @brief Assigns the ReLU function as the activation function for all layers in the net.
+ * @param n The net in question
+ */
+static void assign_relu(net_t& n);
+
+static scalar_datum_t random_scalar();
+
+static void randomize_node(node_wb_t& n);
+
+static void allocate_ws(node_wb_t& n, uint num_weights);
+
+static void allocate_ws(layer_wb_t& l,uint num_weights);
 
 /**
  * @brief Class that provides the bevaviour of a neural network
