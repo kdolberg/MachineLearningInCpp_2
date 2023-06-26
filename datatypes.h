9391b4fbc67bed6644888794a2317e2147be20c5
Kris Dolberg <kristofor.d@gmail.com>
2023-06-26 15:13:51 -0700

    typo fix Adding "MachineLearning::" and "LinearAlgebra" to things that are now in those namespaces Other typename updates (example vector_data_t to col_data_t) Added some member funcs to the activation_function_t struct Updated some comments

--- datatypes.h
+++ datatypes.h
@@ -32,7 +32,7 @@ namespace MachineLearning {
     /**
      * @brief Column in a matrix
      */
-    typedef LinearAlgebra::VecrticalVector<scalar_datum_t> col_t;
+    typedef LinearAlgebra::VerticalVector<scalar_datum_t> col_t;
 
     /**
      * @brief Matrix
@@ -43,8 +43,8 @@ namespace MachineLearning {
      * @brief Weights and biases for a particular layer.
      */
     typedef struct layer_wb_t{
-        matrix_t weights;
-        col_t biases;
+        MachineLearning::matrix_t weights;
+        MachineLearning::col_t biases;
     } layer_wb_t;
 
     /**
@@ -75,140 +75,35 @@ namespace MachineLearning {
      * @param func Activation function
      * @param deriv_func Activation function derivative
      */
-    typedef struct {
+    struct activation_function_s{
         function_t * func;
         function_t * deriv_func;
-        activation_function_t operator++() {
+        activation_function_s operator++() {
             return {this->deriv_func,this->func};
+        };
+        scalar_datum_t operator(scalar_datum_t in) {
+            return this->func(in);
+        };
+        std::vector<scalar_datum_t> operator(const std::vector<scalar_datum_t>& in) {
+            std::vector<scalar_datum_t> ret(in.size());
+            for (LinearAlgebra::uint i = 0; i < in.size(); ++i) {
+                ret[i] = this->func(in[i]);
+            }
+            return this->func(in);
         }
     } activation_function_t;
 
     /**
-     * @brief A dataset formatted for a neural network to train on it.
-     * Each vector_datum_t (elements of the two vectors in the struct) is one discrete datapoint. 
+     * A dataset formatted for a neural network to train on it. Each vector_datum_t (elements of the two vectors in the struct) is one discrete datapoint. 
      * @param y_data Compared to the output of the output layer for use in the error function.
+     * @param x_data Serves as input for the input layer at the start of training.
      */
     typedef struct {
-        /**
-         * @brief Serves as input for the input layer at the start of training.
-         */
-        vector_data_t x_data;
-        /**
-         * @brief 
-         */
-        vector_data_t y_data;
+        col_data_t x_data;
+        col_data_t y_data;
     } xy_dataset_t;
-}
+} //MachineLearning
 
-/**
- * @brief A whole bunch of scalars. Identical in structure to a vector_datum_t, but serves a different purpose. 
- */
-typedef std::vector<scalar_datum_t> scalar_data_t;
-
-/**
- * @brief Stores the input or output dataset for a single layer.
- * Used as input for a layer, input for a node, or output for a layer.
- * Note that this is NOT the output for a node.
- */
-typedef std::vector<vector_datum_t> vector_data_t;
-
-/**
- * @brief Partial derivatives for the weights and biases of a particular node
- */
-typedef node_shape_s<scalar_data_t> node_partial_derivatives_t;
-
-/**
- * @brief Partial derivatives for the weights and biases of a layer
- */
-typedef std::vector<node_partial_derivatives_t> layer_partial_derivatives_t;
-
-/**
- * @brief Partial derivatives for the weights and biases of a net.
- */
-typedef std::vector<layer_partial_derivatives_t> net_partial_derivatives_t;
-
-/**
- * @brief Contains the INPUT data for each layer in a net.
- */
-typedef std::vector<vector_data_t> net_layer_io_data_t;
-
-/**
- * @brief Neural net data that cannot remain const during forpropagation.
- */
-typedef struct {
-    /**
-     * @brief Partial derivatives of base values calculated during forward propagation (?).
-     */
-    net_layer_io_data_t derivs;
-    /**
-     * @brief Intermediate inputs between each layer calculated during forward propagation.
-     */
-    net_layer_io_data_t layer_io;
-
-    /**
-     * @brief Activation data used to calculate IO data and derivatives.
-     */
-    net_layer_io_data_t act;
-} net_for_data_cache_t;
-
-/**
- * @brief Neural net data that cannot remain const during backpropagation.
- */
-typedef struct {
-    net_partial_derivatives_t prelim_partials;
-    /**
-     * @brief Real partial derivatives calculated during backpropagation
-     */
-    net_partial_derivatives_t real_partials;
-} net_back_data_cache_t;
-
-typedef struct {
-    net_for_data_cache_t f;
-    net_back_data_cache_t b;
-} net_all_data_cache_t;
-
-/************************************************************************************************************************
-                                            Structural Neural Net Datatypes
- ************************************************************************************************************************/
-
-
-
-/**
- * @brief Weights and biases used in a particular net. Can also be used as a gradient.
- */
-typedef std::vector<layer_wb_t> net_wb_t;
-
-/**
- * @brief Weights, biases, and activation functions of a net.
- * This struct is intended to include all information in a neural net that can remain const during back and forward propagation.
- */
-typedef struct {
-	/**
-	 * @brief The learning rate
-	 */
-	scalar_datum_t learning_rate;
-	/**
-	 * @brief Weights and biases in each node
-	 */
-	net_wb_t wb;
-	/**
-	 * @brief Activation functions for each node.
-	 */
-	std::vector<activation_function_t> af;
-	/**
-	 * @brief Pointer to training dataset
-	 */
-	const xy_dataset_t * training_dataset;
-
-    /**
-     * @brief Vector originally used to define the net
-     */
-    std::vector<uint> def;
-} net_t;
-
-typedef net_wb_t gradient_t;
-
-#endif
 
 // /**
 //  * @brief A whole bunch of scalars. Identical in structure to a vector_datum_t, but serves a different purpose. 
@@ -317,3 +212,5 @@ typedef net_wb_t gradient_t;
 // } net_t;
 
 // typedef net_wb_t gradient_t;
+
+#endif //DATATYPES_H
